fd934fbee501c78d22a28328bab4a655875a886c
Shakhzod Shokirov <sh.shokirov@ad.openweb.uz>
2024-04-24 19:51:04 +0500

    lessom3 header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
+import { Header } from "./components/Header/Header";
 import "./style/main.css";
-import Header from "./components/Header/Header";
-import { Section } from "./components/Section/Section";
-import Icon from "./assets/react.svg";
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
-      <main>
-        <div>
-          <Section title="Home Page" theme="dark" />
-          <Section title="Content" />
-          <Section title="Info" />
-          <Section title={"12"} dark="dark" />
-        </div>
-        <img src={Icon} alt="" />
-      </main>
-    </>
+    </div>
   );
 }
 export default App;
